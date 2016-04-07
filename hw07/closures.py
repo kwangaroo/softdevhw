eeb0013dd0637b9c567419a18429888a05acd0c7
Kathy Wang <k.wang6898@gmail.com>
2016-04-07 19:06:46 -0400

    how do u even do qs in 1 line and not 3

--- hw07/closures.py
+++ hw07/closures.py
@@ -7,3 +7,15 @@ r2 = repeat("goodbye")
 print r1(2)
 print r2(2)
 print repeat('cool')(3)
+
+
+## quicksort 
+
+def qs(s): 
+    if s == []: 
+        return []
+    return qs([x for x in s[1:] if x < s[0]]) + [s[0]] + qs([x for x in s[1:] if x > s[0]])  
+
+#HOW YALL DOING THIS IN ONE LINE W/O STACK OVERFLOWING
+           
+print qs([3,7,4,8,5,6,2,1])
--- hw08/listcomp.py
+++ hw08/listcomp.py
@@ -20,7 +20,7 @@ def symdiff(a,b):
     return setdiff( union(a,b), intersection(a,b) )
 
 print symdiff([2,4,5,6,7], [4,3,6,9])
-print "expected: [2,3,7,9] i think lol"
+print "expected: [2,3,5,7,9] i think lol"
 
 def cart(a,b): 
     return [(x,y) for x in a for y in b]
